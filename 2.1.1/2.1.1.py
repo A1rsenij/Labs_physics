7ad2a5f957c9cadac60846e55a01f0724898a032
Arseniy Gisich <arseniy.gisich@yandex.ru>
2022-04-04 23:08:19 +0300

    4.04.22

--- 2.1.1/2.1.1.py
+++ 2.1.1/2.1.1.py
@@ -2,43 +2,25 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-data = pd.read_excel('2.3.1.xlsx', sheet_name='Лист1')
-P1 = np.array(data[list(data)[0]]) * 133.3 * 0.0001
-t1 = np.array(data[list(data)[1]])
-k1, cov1 = np.polyfit(t1, P1, 1, cov=True)
-Pv1 = np.polyval(k1, t1)
-data = pd.read_excel('2.3.1.xlsx', sheet_name='Лист2')
-P2 = (np.array(data[list(data)[0]]) - 1.4) * 133.3 * 0.0001
-t2 = np.array(data[list(data)[1]])
-P2 = np.log(P2)
-k2, cov2 = np.polyfit(t2, P2, 1, cov=True)
-Pv2 = np.polyval(k2, t2)
+data = pd.read_excel('2.1.1.xlsx', sheet_name='Лист2')
+names = list(data)
+N1 = np.array(data[names[0]])
+T1 = np.array(data[names[1]])
+k1, cov1 = np.polyfit(N1, T1, 1, cov=True)
+Tp1 = np.polyval(k1, N1)
+
+N2 = np.array(data[names[2]])
+T2 = np.array(data[names[3]])
+k2, cov2 = np.polyfit(N2, T2, 1, cov=True)
+Tp2 = np.polyval(k2, N2)
+
 
 fig, ax = plt.subplots(figsize=(10, 6), dpi=100)
-ax.scatter(t2, P2, label ='Улучшение вакуума', marker='.')
-ax.plot(t2, Pv2, label='$\ln{P} = $' + '{:.6f}$\pm${:.6f} $\cdot$ t + {:.6f}'.format(k2[0], np.sqrt(cov2[0][0]), k2[1]))
 
-ax.legend(loc='upper right', fontsize=10)
-
-ax.grid(which="major", linewidth=0.5)
-ax.grid(which="minor", linestyle='--', linewidth=0.25)
-plt.minorticks_on()
-
-ax.axis([0, 21, -7, -2])
-
-#fig.subplots_adjust(bottom=0.15, left=0.2)
-
-ax.set_title('Зависимость давления $\ln{P}$ от времени $t$', loc='center', fontsize=15)
-ax.set_ylabel('$ln{P} , Па$', loc='center', fontsize=10)
-ax.set_xlabel('$t, с$', loc='center', fontsize=10)
-
-plt.show()
-
-fig.savefig("2.3.1_1.png", dpi=500)
-
-fig, ax = plt.subplots(figsize=(10, 6), dpi=100)
-ax.scatter(t1, P1, label ='Ухудшение вакуума', marker='.')
-ax.plot(t1, Pv1, label='$P = $' + '{:.6f}$\pm${:.6f} $\cdot$ t + {:.6f}'.format(k1[0], np.sqrt(cov1[0][0]), k1[1]))
+ax.scatter(N1, T1, label ='$q = 0,224~г/c$', marker='.')
+ax.plot(N1, Tp1, label='$\Delta{T} = $' + '{:.3f}$\pm${:.3f} $\cdot$ N + {:.3f}'.format(k1[0], np.sqrt(cov1[0][0]), k1[1]))
+ax.scatter(N2, T2, label ='$q = 0,084~г/c$', marker='.')
+ax.plot(N2, Tp2, label='$\Delta{T} = $' + '{:.3f}$\pm${:.3f} $\cdot$ N + {:.3f}'.format(k2[0], np.sqrt(cov2[0][0]), k2[1]))
 
 ax.legend(loc='upper left', fontsize=10)
 
@@ -46,14 +28,14 @@ ax.grid(which="major", linewidth=0.5)
 ax.grid(which="minor", linestyle='--', linewidth=0.25)
 plt.minorticks_on()
 
-ax.axis([0, 51, 0, 0.1])
+ax.axis([0, 1.5, 0, 10])
 
-#fig.subplots_adjust(bottom=0.15, left=0.2)
+fig.subplots_adjust(left=0.07)
 
-ax.set_title('Зависимость давления $P$ от времени $t$', loc='center', fontsize=15)
-ax.set_ylabel('$P , Па$', loc='center', fontsize=10)
-ax.set_xlabel('$t, с$', loc='center', fontsize=10)
+ax.set_title('Зависимость изменения температуры $\Delta{T}$ от мощности $N$', loc='center', fontsize=15)
+ax.set_ylabel('$\Delta{T}, ^{\circ}C$', loc='center', fontsize=10)
+ax.set_xlabel('$N, Вт$', loc='center', fontsize=10)
 
 plt.show()
 
-fig.savefig("2.3.1_2.png", dpi=500)
+fig.savefig("2.1.1.png", dpi=500)
